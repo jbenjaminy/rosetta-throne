11af3bdecbc98522ff2ef7be1eadd35c7fa04693
jbenjaminy <young.bejamin@gmail.com>
2016-08-31 18:33:03 -0500

    finishes algorithm

--- sorting.js
+++ sorting.js
@@ -2,42 +2,159 @@
 //          x  o  x  o  o
 // array = [A, B, C, D, E]
 // var nextQuestion = nextQuestion
-//
+//   questionBank.forEach(function(question) {
+  //   if (question.m === 1) {
+  //     que.push(question)
+  //   }
+  // });
 //
 var sort = function(questionBank) {
   var que = [];
-  questionBank.forEach(function(question) {
-    if (question.m === 1) {
-      que.push(question)
-    }
-  });
   for (var i = 0; i < questionBank.length; i++) {
-    console.log(i, questionBank.length - 1, questionBank[i])
     if (questionBank[i].m === 1) {
       for (var j = i+1; j < questionBank.length; j++) {
         if(questionBank[j].m === 1) {
-          que.push(questionBank[i])
-          que.push(questionBank[j])
-          que.push(questionBank[i])
-          que.push(questionBank[j])
+          que.push(questionBank[i]);
+          que.push(questionBank[j]);
+          que.push(questionBank[i]);
+          que.push(questionBank[j]);
+          i = j;
+          break;
+        } else if (questionBank[j].m === 2) {
+          que.push(questionBank[i]);
+          que.push(questionBank[j]);
+          que.push(questionBank[i]);
+          i = j;
+          break
+        }
+      }
+    } else if (questionBank[i].m === 2) {
+      for (var j = i+1; j < questionBank.length; j++) {
+        if(questionBank[j].m === 1) {
+          que.push(questionBank[j]);
+          que.push(questionBank[i]);
+          que.push(questionBank[j]);
+          i = j;
+          break;
+        } else if (questionBank[j].m === 2) {
+          que.push(questionBank[i]);
+          que.push(questionBank[j]);
+          i = j;
+          break
+        }
+      }
+    } else if (questionBank[i].m === 3 && questionBank[i+1]) {
+      for (var j= i+1; j < questionBank.length; j++ ) {
+        if (questionBank[j].m === 2 && j < questionBank.length - 1) {
+          que.push(questionBank[j]);
+          for (var k = j+1; k < questionBank.length; k++) {
+            if (questionBank[k].m === 1 && questionBank[k+1].m === 1) {
+              que.push(questionBank[k])
+              que.push(questionBank[k+1])
+              que.push(questionBank[i])
+              que.push(questionBank[k])
+              que.push(questionBank[k+1])
+              j = k + 1;
+              break;
+            } else if (questionBank[k].m === 2) {
+              que.push(questionBank[k])
+              que.push(questionBank[i])
+              j = k + 1;
+              break;
+            }
+          }
+          i = j;
+          break;
+        } else if (questionBank[j].m === 3 && j+2 < questionBank.length - 1) {
+          for (var k = j+1; k < questionBank.length; k++) {
+            if (questionBank[k].m === 1 && questionBank[k+1].m === 1) {
+              que.push(questionBank[k])
+              que.push(questionBank[k+1])
+              que.push(questionBank[i])
+              que.push(questionBank[k])
+              que.push(questionBank[k+1])
+              que.push(questionBank[j])
+              j = k + 1;
+              break;
+            } else if (questionBank[k].m === 2 && questionBank[k+1].m === 1) {
+              que.push(questionBank[k+1])
+              que.push(questionBank[i])
+              que.push(questionBank[k])
+              que.push(questionBank[k+1])
+              que.push(questionBank[j])
+              j = k + 1;
+              break;
+            } else if (questionBank[k].m === 2 && questionBank[k+1].m === 2) {
+              que.push(questionBank[i])
+              que.push(questionBank[k])
+              que.push(questionBank[k+1])
+              que.push(questionBank[j])
+              j = k + 1;
+              break;
+            }
+            i = j;
+            break;
+          } 
+        } else if (questionBank[j].m === 3 && j+1 < questionBank.length - 1) {
+            for (var k = j+1; k < questionBank.length; k++) {
+              if (questionBank[k].m === 2 && questionBank[k+1].m === 1) {
+                que.push(questionBank[k])
+                que.push(questionBank[i])
+                que.push(questionBank[k+1])
+                que.push(questionBank[j])
+                que.push(questionBank[k+1])
+                j = k + 1;
+                break;
+              } 
+              else if (questionBank[k].m === 3 && questionBank[k+1].m === 1) {
+                que.push(questionBank[i])
+                que.push(questionBank[k+1])
+                que.push(questionBank[j])
+                que.push(questionBank[k+1])
+                que.push(questionBank[k])
+                j = k + 1;
+                break;
+              }
+            }
+            i = j;
+            break;
+        } else if (j < questionBank.length - 1) {
+          if (questionBank[j+1].m === 1) {
+            que.push(questionBank[j+1])
+            que.push(questionBank[i])
+            que.push(questionBank[j+1])
+            que.push(questionBank[j])
+          } else if (questionBank[j+1].m ===2) {
+            que.push(questionBank[i])
+            que.push(questionBank[j+1])
+            que.push(questionBank[j])
+          }
           i = j;
           break;
         } else {
-          que.push(questionBank[i])
-          que.push(questionBank[i])
+          if (questionBank[j].m === 2) {
+            que.push(questionBank[j])
+            que.push(questionBank[i])
+          } else if (questionBank[j].m === 3) {
+            que.push(questionBank[i])
+            que.push(questionBank[j])
+          }
+          i = j;
+          break;
         }
-      }
-    }
-    if (i === questionBank.length - 1 && questionBank[i].m === 1) {
-      que.push(questionBank[i])
-      que.push(questionBank[i])
-    }
-    for (var i = 0; i < questionBank.length; i++) {
-
+      } 
+    } else if (questionBank[i].m === 3 && i === questionBank.length - 1) {
+        que.push(questionBank[i])
     }
   };
   return que
 }
 
-var array = [{q: 1, m: 1}, {q: 2, m: 1}, {q: 3, m: 1}, {q: 4, m: 1}, {q: 5, m: 1}];
+var array = [{q: 1, m: 4}, {q: 2, m: 4}, {q: 3, m: 4}, {q: 4, m: 4}, {q: 5, m: 4}];
 console.log(sort(array));
+
+ // Bug starts at: [{q: 1, m: 3}, {q: 2, m: 3}, {q: 3, m: 1}, {q: 4, m: 1}, {q: 5, m: 1}];
+
+
+
+
